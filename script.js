4f47689ca700e8412aa45c2828156be35740b998
William Lucht <aaronlucht@gmail.com>
2021-03-25 17:22:30 -0700

    was able to get the color changer

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
-// Pseduo-Code
-
-// What is the header doing
-// the header is displaying the CURRENT TIME (moment.js)
-
-// What is the body doing?
-// we have a list of hours available in 1 hour increments, start at 9am - 5pm (9 hours available)
-// We have 3 different colors 
-    // IF hour is LESS THAN current time make it gray
-    // IF hour is EQUAL to the current time display it red
-    // IF hour is GREATER THAN the current time display it green
-
-    // We have classes set for 'past', 'present', 'future' for the color related to time
-
-//What happens when I enter something into the field?
-// we want to store the inputs in localstorage
-// IF the save button is clicked AND the data is stored, display the contents of the input
-
-// make buttons do stuff
-    // when button is clicked, save to localstorage
-// make boxes change color
-// add input text (append) to ?
-// retrieve values from localstorage and show them
-
-// display current day in header
-
+var currentTimeHour = moment().format("LT");
 var hourSelect = document.querySelectorAll('hour');
+var timeBlock = $(".time-block");
 var hour9 = $("#hour-9");
 var hour10 = $("#hour-10");
 var hour11 = $("#hour-11");
@@ -35,6 +11,10 @@ var hour15 = $("#hour-15");
 var hour16 = $("#hour-16");
 var hour17 = $("#hour-17");
 var saveButton = $(".saveBtn");
+var hourArray = $(".time-block");
+
+// console.log(hourArray);
+// console.log(dataHourArray);
 
 function displayTime(){
     var headMoment = moment().format('LLL'); 
@@ -44,30 +24,46 @@ function displayTime(){
 
 function colorShift(){
     var currentHour = moment().hour();
+    // var currentHour = 17;
     console.log(currentHour);
 
     // we need an IF statement to set colors based on the current time
-    if(currentHour <= 8 || currentHour >= 17){
-
-    };
+    for (var i = 9; i <= 17; i++) {
+        if (currentHour > [i] ) {
+            $("#hour-"+[i]).addClass("past");
+        } else if (currentHour == [i] ) {
+            $("#hour-"+[i]).addClass("present");
+        } else if (currentHour < [i] ) {
+            $("#hour-"+[i]).addClass("future");
+        }
+    }    
+    // for (var i = 0; hourArray.length; i++){
+    //     if(currentHour > $(hourArray[i]).attr("data-hour")){
+    //         $(hourArray[i]).addClass("past");
+    //     }
+    // }
 };
 
+// working - DO NOT CHANGE
 saveButton.on("click", function(event){
-    // we need to tie an event to the onclick event for the button
     var getText = $(this).siblings(".description").val();
-    console.log(getText);
-    // get stored text from localstorage
-    // get the parent id we are in when we enter text
     var getId = $(this).parent().attr("id");
-    // console.log(getId);
-    // we want to set the ID and its content to localstorage
     localStorage.setItem(getId, getText);
-
-    // var setLocalStorage = document.localstorage.setItem("saved-text", getText);
-    // console.log(setLocalStorage);
 });
+
+// get items from localstorage
+// var retrievedObject = localStorage.getItem(getId, getText);
+//     console.log(retrievedObject);
+// // get items from local storage
+// function displayStored(){
+//     var retrievedObject = localStorage.getItem(getId, getText);
+//     console.log(retrievedObject);
+// };
+// displayStored();
+
+
 
 displayTime();
 setInterval(displayTime, 1000);
 
-// colorShift();
+colorShift();
